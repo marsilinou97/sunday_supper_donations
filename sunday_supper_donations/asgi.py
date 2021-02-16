ff82f85ceee684a82d74bf261951afb66c0148a9
marsilinou97 <marsilinou.97@gmail.com>
2021-02-16 14:07:10 -0800

    Initial push

---
+++ sunday_supper_donations/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sunday_supper_donations project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sunday_supper_donations.settings')
+
+application = get_asgi_application()
---
+++ sunday_supper_donations/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sunday_supper_donations project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sunday_supper_donations.settings')
+
+application = get_wsgi_application()
