2b812f6e2e38103c1459b7d3d9ae9759b9fd2414
rv-ben <ruben.bramasco@student.csulb.edu>
2021-02-20 21:44:35 -0800

    Adds donar info form

---
+++ input/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+
+class DonarInformationForm(forms.Form):
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
+    email = forms.EmailField(required=False)
+    dob = forms.DateField(required=False)
+    address1 = forms.CharField(required=False)
+    address2 = forms.CharField(required=False)
+    city = forms.CharField(required=False)
+    state = forms.CharField(required=False)
+    zip = forms.CharField(required=False)
+
+    class Meta:
+        # model = input
+        fields = ['first_name', 'last_name', 'dob', 'address1','address2', 'city', 'state', 'zip', 'email']
+
+    def __init__(self, *args, **kwargs):
+
+        super(DonarInformationForm, self).__init__(*args, **kwargs)
+
+        fields = self.visible_fields()
+        for visible in fields:
+            # Add class to each of the form elements
+            visible.field.widget.attrs['class'] = 'form-control'
+
+        self.fields["first_name"].widget.attrs.update({"placeholder": "First Name"})
+        self.fields["last_name"].widget.attrs.update({"placeholder": "Last Name"})
+        self.fields["dob"].widget.attrs.update({"placeholder": "D.O.B"})
+        self.fields["address1"].widget.attrs.update({"placeholder": "Address Line 1"})
+        self.fields["address2"].widget.attrs.update({"placeholder": "Address Line 2"})
+        self.fields["city"].widget.attrs.update({"placeholder": "City"})
+        self.fields["state"].widget.attrs.update({"placeholder": "State"})
+        self.fields["zip"].widget.attrs.update({"placeholder": "Zip Code"})
+        self.fields["email"].widget.attrs.update({"placeholder": "Email"})
+
--- input/views.py
+++ input/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 
+from .forms import DonarInformationForm
+
 
 # Create your views here.
+def handle_get_req(request):
+    form = DonarInformationForm()
+    return render(request, 'input/donar-info-form.html', {'form': form})
+
+def handle_post_req(request):
+    form = DonarInformationForm(request.POST)
+    print(list(request.POST.items()))
+
+    return render(request, 'input/donar-info-form.html')
+
 def index(request):
-    return render(request, 'input/input_main.html')
+    if request.method == 'POST':
+        return handle_post_req(request)
+
+    form = DonarInformationForm()
+    return render(request, 'input/donar-info-form.html', {'form': form})
