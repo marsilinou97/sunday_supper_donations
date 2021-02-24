07cfa8f6ec9e065a7ef429f616f38f9d2d9c9043
marsilinou97 <marsilinou.97@gmail.com>
2021-02-24 01:37:16 -0800

    Added logo/styling to static, added new link to nav bar

--- sunday_supper_donations/settings.py
+++ sunday_supper_donations/settings.py
@@ -123,4 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# TODO: change to prod dir
 STATIC_URL = '/static/'
+ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),]
