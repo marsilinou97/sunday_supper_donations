ef5f70ba3bb27ad2f0e70175ccd70f9717e30a59
aloafofbrad <aloafofbrad2@gmail.com>
2021-02-25 13:11:31 -0800

    Added function to filter results, added query to select all Food Items

--- analytics/queries.py
+++ analytics/queries.py
@@ -1,9 +1,47 @@
 from django.db import models
 from input.models import *
+import sys # debugging
 
 """
 Not sure if this is a good place to put this code, if you need to move it feel free. -Brad
 """
+
+"""
+Filters results. Just a quick implementation, there may be more efficient ways to do this,
+but using this method means the db doesn't need to be queried every time the filter is changed.
+
+Args:
+    results: a list of dictionaries
+    filters: a dictionary, where each key:value pair is searched for
+
+Example:
+    FilterResults(
+                    results,
+                    filters={"first_name":"John"}
+                )
+
+    This should strip the results of any
+"""
+def FilterResults(
+                    results,
+                    filters={}
+                    ):
+    if filters != {}:
+        i = 0
+        while i < len(results):
+            removed = False
+            for key in filters:
+                try:
+                    if not removed and results[i][key] != filters[key]:
+                        results.remove(results[i])
+                        removed = True
+                except:
+                    print(sys.exc_info()[0]) # debugging
+                    print(sys.exc_info()[1])
+                    print(sys.exc_info()[2])
+            if not removed:
+                i += 1
+    return results
 
 """
 Query the db to get Funds that were donated and all relevant info
@@ -94,6 +132,42 @@ def SelectAllClothings():
         # Not all of these try blocks are necessary; just in case.
         try:
             b = Item.objects.get(id=clothing.items_id.id)
+            row["quantity"] = b.quantity
+        except:
+            print("Item doesn't exist")
+        try:
+            c = Donation.objects.get(id=b.donations_id.id)
+            row["date_received"] = str(c.date_received)
+        except:
+            print("Donation doesn't exist")
+        try:
+            d = Donor.objects.get(id=c.donor.id)
+            print(d)
+        except:
+            print("Donor doesn't exist")
+
+        names = FindDonorNames(d)
+        row["first_name"] = names[0]
+        row["last_name"] = names[1]
+
+        results.append(row)
+    return results
+
+"""
+Query the db to get Food Items that were donated and all relevant info
+Returns a list of dictionaries with the following fields:
+    name, quantity, date_received, first_name, last_name
+"""
+def SelectAllFood():
+    results = []
+    a = Food.objects.all()
+    for food in a:
+        row = {}
+        row["misc_name"] = food.name
+
+        # Not all of these try blocks are necessary; just in case.
+        try:
+            b = Item.objects.get(id=food.items_id.id)
             row["quantity"] = b.quantity
         except:
             print("Item doesn't exist")
