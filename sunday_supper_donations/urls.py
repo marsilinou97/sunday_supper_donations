0da924789d7e6870fa6c546534f0cc9b95f8c2bb
aloafofbrad <aloafofbrad2@gmail.com>
2021-03-09 21:22:54 -0800

    Added url routing for login/logout

--- sunday_supper_donations/urls.py
+++ sunday_supper_donations/urls.py
@@ -20,12 +20,16 @@ from input import views as input_views
 from news import views as news_views
 from users import views as users_views
 from analytics import views as analytics_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', users_views.register, name="register"),
     path('input/', input_views.index, name="input_page"),
     path('admin/', admin.site.urls, name="admin"),
     path('register/', users_views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
+    path('profile/', users_views.profile, name="profile"),
     path('news/', news_views.index, name="news"),
     # adjusted analytics url routing
     path('analytics/', include('analytics.urls')),
