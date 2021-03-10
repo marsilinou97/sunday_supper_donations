87414630f5bea3d308a8f5600487934efd5b8930
inTheGrey <59034202+neytMont@users.noreply.github.com>
2021-03-10 12:31:55 -0800

    update
    fixed some minor bugs

--- input/views.py
+++ input/views.py
@@ -26,6 +26,7 @@ def handle_get_req(request):
     form = DonorInformationForm()
     return render(request, 'input/donor-info-form.html', {'form': form})
 
+
 def handle_post_req(request):
     # Save the data that the user entered
     user_input = {}  # user input dictionary
@@ -35,29 +36,27 @@ def handle_post_req(request):
     #     cur_user = request.User.username
     # user_input_items = {}  # items dictionary
     items_list = []  # list of dictionary for item types
-    data = request.POST.dict() # Get request.POST as a regular dictionary
+    data = request.POST.dict()  # Get request.POST as a regular dictionary
     print(data)
 
     # Get information about Funds, if any were donated
     type = None
     amount = None
+    send_thanks = False
     if "type" in data:
         type = data["type"]
     if "amount" in data:
         amount = data["amount"]
+    if "thanks_sent" in data:
+        if data["thanks_sent"] is "on":
+            send_thanks = True
 
     # If there was a Fund donated, add it to the list
     if type != None and type != "" and amount != None:
-        items_list.append({"subclass":Fund,"quantity":1,"fundTypeName":type,"amount":amount})
-
-    # Get the total number of items
-    number_items = 0
-    if "form-TOTAL_FORMS" in data:
-        number_items = data["form-TOTAL_FORMS"]
-
+        items_list.append({"subclass": Fund, "quantity": 1, "fundTypeName": type, "amount": amount})
     # Iterate over all item-related keys from request.POST
     i = 0
-    next_key = 'id_form-0-type' # a.k.a.: 'id_form-' + str(i) + '-type'
+    next_key = 'id_form-' + str(i) + '-type'  # a.k.a.: 'id_form-' + str(i) + '-type'
     while next_key in data:
         """
         This loop condition should work for all items in the donation since all
@@ -71,23 +70,24 @@ def handle_post_req(request):
         item_dict["subclass"] = data['id_form-' + str(i) + '-type']
         if item_dict['subclass'] == 'giftcard':
             item_dict['subclass'] = GiftCard
-            item_dict['amount'] = -999 # TODO: fix once the form has an amount for Giftcards
-
+            item_dict['amount'] = data['id_form-' + str(i) + '-amount']  # TODO: fix once the form has an amount for Giftcards
             # get the Giftcard enumerated value
             if 'id_form-' + str(i) + '-sub_type_business' in data:
                 item_dict['businessName'] = data['id_form-' + str(i) + '-sub_type_business']
             else:
                 item_dict['businessName'] = ""
+
         elif item_dict['subclass'] == 'clothing':
             item_dict["subclass"] = Clothing
-
             # get the Clothing enumerated value
-            if 'id_form-' + str(i + 1) + '-sub_type_clothing' in data:
-                item_dict['clothingTypeName'] = data['id_form-' + str(i + 1) + '-sub_type_clothing']
-            else:
-                item_dict['clothingTypeName'] = "men"
+            if 'id_form-' + str(i-1) + '-sub_type_clothing' in data:
+                item_dict['clothingTypeName'] = data['id_form-' + str(i-1) + '-sub_type_clothing']
+            # else:
+            #    item_dict['clothingTypeName'] = "men"
+
         elif item_dict['subclass'] == 'food':
             item_dict["subclass"] = Food
+
         elif item_dict['subclass'] == 'misc':
             item_dict["subclass"] = Miscellaneous
 
@@ -110,14 +110,14 @@ def handle_post_req(request):
     if "anonymous" in user_input:
         if user_input['anonymous'] == 'on':
             # If it was, select the anonymous donor from the db
-            donor = Donor.objects.get(first_name="ANONYMOUS",last_name="ANONYMOUS",email="")
+            donor = Donor.objects.get(first_name="ANONYMOUS", last_name="ANONYMOUS", email="")
 
     # If it wasn't anonymous, or if the anonymous donor wasn't in the db,
     # Try to insert the donor.
     if donor == None:
         # Get donor information from the POST request
-        user_keys = ["first_name","last_name","email_address","phone_number","state",
-        "city","zip","address1","address2"]
+        user_keys = ["first_name", "last_name", "email_address", "phone_number", "state",
+                     "city", "zip", "address1", "address2"]
         for key in user_keys:
             # If the data exists, great. Use it
             if key in data:
@@ -142,110 +142,37 @@ def handle_post_req(request):
     # Should technically work even if items_list == []
     try:
         # InsertDonation(new_donor, items_list, user_input['date_received'], user_input['thanks_sent'], cur_user, "None")
-        InsertDonation(donor, items_list, data['date_received'], True , cur_user, "None")
+        InsertDonation(donor, items_list, data['date_received'], send_thanks, cur_user, data['comment'])
     except:
         for error in sys.exc_info():
             print(error)
 
+    """
+    ===================================================================================================
+        TEST CODE : Making sure that every value is in the right key.
+    ===================================================================================================
+    """
+    print('date_received: ' + data['date_received'] + " |||| " )
+    print('thanks_sent: ' + str(send_thanks) + " |||| ")
+    print('comments: ' + data['comment'] + " |||| ")  # its comments on models.py
+    print('type: ' + data['type'] + " ||||||| " + type)
+    print('amount: ' + data['amount'] + " |||||||| " + amount)
+    print('first_name: ' + data['first_name'] + " |||| " + user_input['first_name'])
+    print('last_name: ' + data['last_name'] + " |||| " + user_input['last_name'])
+    print('email: ' + data['email'] + " |||| " + user_input['email_address'])
+    print('phone_number: ' + data['phone_number'] + " |||| " + user_input['phone_number'])
+    print('address_line1: ' + data['address1'] + " |||| " + user_input['address1'])
+    print('address_line2: ' + data['address2'] + " |||| " + user_input['address2'])
+    print('city: ' + data['city'] + " |||| " + user_input['city'])
+    print('state: ' + data['state'] + " |||| " + user_input['state'])
+    print('zipcode: ' + data['zip'] + " |||| " + user_input['zip'])
+
+
+
+
+
     messages.success(request, 'Donation Saved.')
     return redirect('input_page')
-
-    # number_items = 0
-    # count = 0  # controller
-    #
-    # for a in request.POST:  # loops the input and populates the dictionary
-    #     user_input[a] = request.POST[a]
-    #     count += 1
-    #     user_input['comment'] = "NEEDS WORK"
-    #     if count == 15:  # to get the amount on item in the user input
-    #         number_items = int(user_input['form-TOTAL_FORMS'])
-    #     elif count >= 19:  # if the input is on the items
-    #         user_input_items[a] = request.POST[a]  # put the data on another dictionary
-    #
-    # user_input_fundTypeName = user_input['type']
-    # user_input_fund_amount = user_input['amount']
-    #
-    # print(user_input)
-    # print(user_input_items)
-    #
-    # # the fund type and amount, needs to be on the first
-    # items_list.append({})
-    # items_list[0]['quantity'] = 1
-    # items_list[0]['subclass'] = Fund
-    # items_list[0]['amount'] = user_input_fund_amount
-    # items_list[0]['fundTypeName'] = user_input_fundTypeName
-
-    # another for loop that will put the user_input_items in the items_list in a dictionary with 3 pairs
-
-    # for i in range(0, number_items):
-    #     item_dic = {}
-    #     for a in range(0, len(user_input_items)):
-    #         item_dic['type'] = user_input_items['id_form-' + str(i) + '-type']
-    #
-    #         if item_dic['type'] == 'giftcard':
-    #             item_dic['subclass'] = GiftCard
-    #             item_dic['amount'] = -999 # TODO: fix once the form has an amount for Giftcards
-    #         elif item_dic['type'] == 'clothing':
-    #             item_dic["subclass"] = Clothing
-    #         elif item_dic['type'] == 'food':
-    #             item_dic["subclass"] = Food
-    #         elif item_dic['type'] == 'misc':
-    #             item_dic["subclass"] = Miscellaneous
-    #
-    #         item_dic['quantity'] = user_input_items['id_form-' + str(i) + '-quantity']
-    #
-    #         # Giftcard
-    #         if 'id_form-' + str(i) + '-sub_type_business' in user_input_items:
-    #             item_dic['businessName'] = user_input_items['id_form-' + str(i) + '-sub_type_business']
-    #
-    #         # Clothing
-    #         elif 'id_form-' + str(i) + '-sub_type_clothing' in user_input_items:
-    #             item_dic['clothingTypeName'] = user_input_items['id_form-' + str(i) + '-sub_type_clothing']
-    #
-    #         # Food/Misc
-    #         elif 'id_form-' + str(i) + '-sub_type_name' in user_input_items:
-    #             item_dic['name'] = user_input_items['id_form-' + str(i) + '-sub_type_name']
-    #     items_list.append(item_dic)
-    # for i in range(0,number_items):
-    #     temp_item = {}
-    #     num_items_in_dic = 0
-    #     for a in user_input_items:
-    #         temp_item[a] = user_input[a]
-    #         num_items_in_dic += 1
-    #         if num_items_in_dic == 3:
-    #             break
-    #     items_list.append(temp_item)
-
-    # for a in items_list:
-    #     print(a)
-    #     a["key"] = "value"
-
-    """
-    # populating items_list with items thats not funds
-    """
-    # for i in range(0,number_items):
-    #     item_dic = {}
-    #     for a in range(0,len(user_input)):
-    #         item_dic['type'] = user_input['id_form-' + str(a) + '-type']
-    #         item_dic['quantity'] = user_input['id_form-' + str(a) + '-quantity']
-    #
-    #         # Giftcard
-    #         if 'id_form-' + str(a) + '-sub_type_business' in user_input:
-    #             item_dic['businessName'] = user_input['id_form-' + str(a) + '-sub_type_business']
-    #
-    #         # Clothing
-    #         elif 'id_form-' + str(a) + '-sub_type_clothing' in user_input:
-    #             item_dic['clothingTypeName'] = user_input['id_form-' + str(a) + '-sub_type_clothing']
-    #
-    #         # Food/Misc
-    #         elif 'id_form-' + str(a) + '-sub_type_name' in user_input:
-    #             item_dic['name'] = user_input['id_form-' + str(a) + '-sub_type_name']
-    #     items_list.append(item_dic)
-    # This one is for fund types
-
-    # items_list.append({})
-
-
 
 
 
