2b2271d7aa1374949e7c2fb41773e07b2454ea16
Rv-ben <rubenbramasco@student.csulb.edu>
2021-03-11 10:46:10 -0800

    Recreates rawdata query usinf django

--- analytics/queries.py
+++ analytics/queries.py
@@ -3,7 +3,9 @@ from input.models import *
 import sys  # debugging
 from django.db import connection
 from django.db.models import F
-
+from analytics.vars import *
+import functools
+from itertools import chain
 """
 Not sure if this is a good place to put this code, if you need to move it feel free. -Brad
 """
@@ -298,8 +300,35 @@ Return type is a dictionary.
 #     return counts
 
 
-def get_raw_data():
-    return Fund.objects.select_related('item__donation__donor').annotate(test=F('item__quantity')).values('test').all()
+def get_model_raw_data_query(model: models.Model, item_specific_fields: dict):
+
+    select_fields = dict(RAW_DATA_BASE_FIELDS_KEYS)
+    select_fields.update(item_specific_fields)
+
+    fields_keys = list(RAW_DATA_BASE_FIELDS_KEYS.keys())
+    fields_keys.extend(item_specific_fields.keys())
+
+    query = model.objects.prefetch_related('item').annotate(
+        **select_fields
+    ).values(
+        *fields_keys
+    )
+    
+    #query = model.objects.prefetch_related('item').values("item__quantity")
+    return query
+
+def union_queries(list_of_queries: list):
+    return  list_of_queries[0].union(*list_of_queries[1:], all=True)
+
+def get_raw_data_query():
+    queries = []
+    for query in RAW_DATA_QUERIES:
+        queries.append(get_model_raw_data_query(
+            query["MODEL"],
+            query["FIELDS"]
+        ))
+    return union_queries(queries) 
+
 
 def execute_fetch_raw_query(query, fetch_all=False, fetch_one=False, params={}):
     if fetch_all or fetch_one:
--- analytics/views.py
+++ analytics/views.py
@@ -47,7 +47,9 @@ def raw_data(request):
     form = RawDataForm()
 
     if request.method == 'GET':
-        print(get_raw_data())
+        #raw_query = get_raw_data_query().all()
+        print(get_raw_data_query())
+        #print(raw_query.filter(item_type='Fund'))
 
         tables_data = get_raw_page_tables_data(raw_data_query)
         context = {
@@ -58,7 +60,7 @@ def raw_data(request):
             "giftcards": tables_data['giftcards'],
             "miscellaneous": tables_data['miscellaneous']
         }
-        return render(request, 'analytics/rawdata.html', context)
+        return render(request, 'analytics/rawdata.html',context)
 
     elif request.method == 'POST':
         # Handles POST request
