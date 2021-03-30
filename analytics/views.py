36f2d5f89aaf356961aaa99d5905aeec60aa2c0b
Efrain Huizar <efrainfhuizar@gmail.com>
2021-03-30 13:40:46 -0800

    Connected charts and cleaned up old code

--- analytics/views.py
+++ analytics/views.py
@@ -18,112 +18,30 @@ get_zeros_list = lambda n: [0] * n
 
 
 def index(request):
+
     if request.method == 'GET':
-        pie_context = get_pie_chart_context(pie_chart_query)
-        context = get_line_chart_context(lines_charts_query)
-        context.update(pie_context)
 
         form = ChartsForm()
 
-        context.update({"form": form})
-
-        cursor = connection.cursor()
-
-        # Get all donation data for each chart
-        cursor.execute(fund_chart_query)
-        fund = dictfetchall(cursor)
-        fund_data = {"fund_data": fund}
-
-        cursor.execute(giftcard_chart_query)
-        gift = dictfetchall(cursor)
-        giftcard_data = {"giftcard_data": gift}
-
-        cursor.execute(clothing_chart_query)
-        cloth = dictfetchall(cursor)
-        clothing_data = {"clothing_data": cloth}
-
-        cursor.execute(food_chart_query)
-        food = dictfetchall(cursor)
-        food_data = {"food_data": food}
-
-        cursor.execute(miscellaneous_chart_query)
-        misc = dictfetchall(cursor)
-        miscellaneous_data = {"miscellaneous_data": misc}
-
-        context.update(fund_data)
-        context.update(giftcard_data)
-        context.update(clothing_data)
-        context.update(food_data)
-        context.update(miscellaneous_data)
+        context = {"form": form}
 
         return render(request, 'analytics/analytics.html', context)
 
-    elif request.method == 'POST':
-        form = RawDataForm(request.POST)
-
-        if form.is_valid():
-            filters, pie_chart_filter_query = get_filters_and_query(form, pie_chart_query)
-            filters, lines_chart_filter_query = get_filters_and_query(form, lines_charts_query)
-
-            if not filters:
-                # If no filters supplied redirect to the raw data page
-                return redirect("analytics_rawdata")
-
-            pie_context = get_pie_chart_context(pie_chart_filter_query)
-            context = get_line_chart_context(lines_chart_filter_query)
-            context.update(pie_context)
-
-            form = RawDataForm()
-            context.update({"form": form()})
-            return render(request, 'analytics/analytics.html', context)
-
-        else:
-            HttpResponse("Error...")
+    else:
+        return HttpResponse("ERROR...")
 
 def raw_data(request):
-    form = RawDataForm()
+    if request.method == "GET":
 
-    if request.method == 'GET':
+        form = RawDataForm()
 
-        #tables_data = get_raw_page_tables_data(raw_data_query)
-        context = {
-            "form": form,
-            "funds": tables_data['funds'],
-            "clothing": tables_data['clothing'],
-            "foods": tables_data['foods'],
-            "giftcards": tables_data['giftcards'],
-            "miscellaneous": tables_data['miscellaneous']
-        }
+        context = {"form": form}
+
+
         return render(request, 'analytics/rawdata.html', context)
 
-    elif request.method == 'POST':
-        # Handles POST request
-        form = RawDataForm(request.POST)
-
-        if form.is_valid():
-            #filters, query_filter = get_filters_and_query(form, filter_query)
-
-            if not filters:
-                # If no filters supplied redirect to the raw data page
-                return redirect("analytics_rawdata")
-
-            #tables_data = get_raw_page_tables_data(query_filter, filters)
-
-            context = {
-                "form": form,
-                "funds": tables_data['funds'],
-                "clothing": tables_data['clothing'],
-                "foods": tables_data['foods'],
-                "giftcards": tables_data['giftcards'],
-                "miscellaneous": tables_data['miscellaneous']
-            }
-
-        # Return populated context based on HTTP request
-        return render(request, 'analytics/rawdata.html', context)
     else:
-        HttpResponse("Error...")
-
-        HttpResponse("Error...")
+        return HttpResponse("ERROR...")
 
 def delete_fund(request):
     fund_id = request.POST["fund_id"]
@@ -167,6 +85,7 @@ def get_table(request):
         return HttpResponse("ERROR...")
 
 def get_donation_count_date_qty(request):
+
     if (request.method == "GET"):
         
         json_response = {}
@@ -179,7 +98,7 @@ def get_donation_count_date_qty(request):
                 qty_count_by_month[row["month"]-1] += row["qty"]
             
             json_response.update({
-                key : qty_count_by_month
+                key: qty_count_by_month
             })
 
         return JsonResponse(json_response, safe= False)
@@ -197,7 +116,7 @@ def get_donation_count_month(request):
                 qty_count_by_month[row["month"]-1] += row["count"]
             
             json_response.update({
-                key : qty_count_by_month
+                key: qty_count_by_month
             })
 
         return JsonResponse(json_response, safe= False)
@@ -211,7 +130,7 @@ def get_donation_item_count(request):
             results = get_total_donation_count_qty(QUERY_DATA[key]["MODEL"])
             
             json_response.update({
-                key : results["qty"]
+                key: results["qty"]
             })
 
         return JsonResponse(json_response, safe= False)
@@ -227,7 +146,7 @@ def get_donation_fund_count(request):
             print(results)
 
             json_response.update({
-                fund_type.name : results
+                fund_type.name: results
             })
 
 
