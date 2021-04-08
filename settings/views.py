55f137b49410fd187b10a420a644f814db6411b0
marsilinou97 <marsilinou.97@gmail.com>
2021-04-08 01:51:31 -0700

    Updated user password update

--- settings/views.py
+++ settings/views.py
@@ -9,44 +9,46 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def change_password(request):
     if request.method == 'POST':
-        print(request.POST + '\n\n\n\n')
+        # print(request.POST + '\n\n\n\n')
         form = PasswordChangeForm(request.user, request.POST)
-        print(form)
-    return render(request, "settings/change_password.html")
-    """
+        # print(form)
+        # return render(request, "settings/change_password.html")
+
         if form.is_valid():
             user = form.save()
             update_session_auth_hash(request, user)  # Important!
             messages.success(request, 'Password changed')
             return redirect('change_password')
         else:
-            messages.error(request, 'Please correct the error below.')
-    else:
-        form = PasswordChangeForm(request.user)
-    """
+            print(f"Error updating password {form.error_messages}")
+            messages.error(request, form.errors)
 
+    return render(request, "settings/change_password.html")
 
-    # return render(request, "settings/change_password.html", {'form': form})
 
 @login_required
 def manage_roles(request):
     if request.user.is_authenticated:
-        return render(request,"settings/manage_roles.html")
+        return render(request, "settings/manage_roles.html")
+
 
 @login_required
 def manage_registration_links(request):
     if request.user.is_authenticated:
-        return render(request,"settings/manage_registration_links.html")
+        return render(request, "settings/manage_registration_links.html")
+
 
 @login_required
 def manage_registration_links_get_table(request):
     if request.user.is_authenticated:
-        return render(request,"settings/manage_registration_links.html")
+        return render(request, "settings/manage_registration_links.html")
+
 
 @login_required
 def help(request):
     if request.user.is_authenticated:
-        return render(request,"settings/help.html")
+        return render(request, "settings/help.html")
+
 
 @login_required
 def index(request):
