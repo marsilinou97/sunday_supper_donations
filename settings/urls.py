6f9800718fa0d234a9eddcda119f97020950bb14
aloafofbrad <aloafofbrad2@gmail.com>
2021-04-07 17:52:59 -0700

    Updated url routing for various settings pages

--- settings/urls.py
+++ settings/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='settings_index'),
+    path('change_password/', views.change_password, name='change_password'),
+    path('manage_roles/', views.manage_roles, name='manage_roles'),
+    path('manage_registration_links', views.manage_registration_links, name='manage_registration_links'),
+    path('help/', views.help, name='help')
 ]
--- settings/views.py
+++ settings/views.py
@@ -2,8 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 @login_required
 def index(request):
     if request.user.is_authenticated:
         return render(request,"settings/settings.html")
+
+@login_required
+def change_password(request):
+    if request.user.is_authenticated:
+        return render(request,"settings/change_password.html")
+
+@login_required
+def manage_roles(request):
+    if request.user.is_authenticated:
+        return render(request,"settings/manage_roles.html")
+
+@login_required
+def manage_registration_links(request):
+    if request.user.is_authenticated:
+        return render(request,"settings/manage_registration_links.html")
+
+@login_required
+def help(request):
+    if request.user.is_authenticated:
+        return render(request,"settings/help.html")
