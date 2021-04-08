969946396c8e97aca622df2eb1875f3ee62f6e8c
marsilinou97 <marsilinou.97@gmail.com>
2021-04-08 16:09:03 -0700

    Fix LOGIN_REDIRECT_URL path

--- sunday_supper_donations/settings.py
+++ sunday_supper_donations/settings.py
@@ -135,5 +135,5 @@ ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'), ]
 
-LOGIN_REDIRECT_URL = 'input'
+LOGIN_REDIRECT_URL = 'input_index'
 LOGOUT_REDIRECT_URL = 'login'
