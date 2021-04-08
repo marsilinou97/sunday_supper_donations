b2347a6402bac2e70bdf28bc6d1dcd0703eaf859
inTheGrey <59034202+neytMont@users.noreply.github.com>
2021-04-07 22:52:10 -0700

    change_password

--- settings/views.py
+++ settings/views.py
@@ -1,16 +1,32 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-@login_required
-def index(request):
-    if request.user.is_authenticated:
-        return render(request,"settings/settings.html")
 
 @login_required
 def change_password(request):
-    if request.user.is_authenticated:
-        return render(request,"settings/change_password.html")
+    if request.method == 'POST':
+        print(request.POST + '\n\n\n\n')
+        form = PasswordChangeForm(request.user, request.POST)
+        print(form)
+    return render(request, "settings/change_password.html")
+    """
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Password changed')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    """
+
+
+    # return render(request, "settings/change_password.html", {'form': form})
 
 @login_required
 def manage_roles(request):
@@ -26,3 +42,12 @@ def manage_registration_links(request):
 def help(request):
     if request.user.is_authenticated:
         return render(request,"settings/help.html")
+
+@login_required
+def index(request):
+    if request.method == 'POST':
+        return change_password(request)
+    """
+    if request.user.is_authenticated:
+        return render(request,"settings/settings.html")
+    """
