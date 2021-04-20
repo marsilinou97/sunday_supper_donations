8fa30084788c5bbf951f08705e63f974a19745b6
Efrain Huizar <efrainfhuizar@gmail.com>
2021-04-19 20:01:56 -0800

    Efrn design changes

--- analytics/views.py
+++ analytics/views.py
@@ -1,53 +1,54 @@
+from math import ceil
+from typing import Dict
+from django.db.models.query_utils import Q
+
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import redirect
+from django.shortcuts import render
+from django.db.models import Sum
+from django.db.models.functions import Extract
+import datetime
 import json
 import traceback
-
-from django.contrib.auth.decorators import login_required
-from django.db import transaction
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
-
 from helpers import FailedJsonResponse
-from helpers import remove_html_tags
-from input.forms import FundsForm, ItemForm
 from .forms import RawDataForm, ChartsForm
+
 from .queries import *
 from .vars import *
 
 get_zeros_list = lambda n: [0] * n
 
 
-# TODO replace login decorators with middleware
-@login_required(login_url="login")
 def index(request):
     if request.method == 'GET':
+
         form = ChartsForm()
+
         context = {"form": form}
+
         return render(request, 'analytics/analytics.html', context)
+
     else:
         return HttpResponse("ERROR...")
 
 
-@login_required(login_url="login")
 def raw_data(request):
     if request.method == "GET":
+
         form = RawDataForm()
+
         context = {"form": form}
+
         return render(request, 'analytics/rawdata.html', context)
+
     else:
         return HttpResponse("ERROR...")
 
 
-@login_required(login_url="login")
 def edit_donations(request):
-    # forms = {
-    #     'donor_form': DonorInformationForm(),
-    #     'donation_info_form': DonationForm(),
-    #     'funds_form': FundsForm(),
-    # }
-    return render(request, 'analytics/edit_donations.html', {'funds_form_types': FundsForm(), 'item_form': ItemForm()})
+    return render(request, 'analytics/edit_donations.html')
 
 
-@login_required(login_url="login")
 def get_table(request):
     # Get request parse
     if request.method == "GET":
@@ -55,13 +56,6 @@ def get_table(request):
             model = request.GET["table_type"]
             offset = int(request.GET["offset"])
             limit = int(request.GET["limit"])
-
-            search_keyword = request.GET["search"]
-            order_direction = request.GET["order"]  # asc, desc
-            order_by = request.GET["sort"]  # column name
-
-            if order_direction == "desc" and order_by:
-                order_by = "-" + order_by
 
             query_info = QUERY_DATA[model]
             rows_count = query_info["MODEL"].objects.count()
@@ -73,8 +67,7 @@ def get_table(request):
             if offset > rows_count:
                 raise ValueError
 
-            query_set = get_model_raw_data_query(query_info["MODEL"], query_info["RAW_DATA_FIELDS"], offset, limit,
-                                                 order_by, search_keyword)
+            query_set = get_model_raw_data_query(query_info["MODEL"], query_info["RAW_DATA_FIELDS"], offset, limit)
             json_response = {"rows": list(query_set), "total": query_info["MODEL"].objects.count()}
 
             return JsonResponse(json_response, safe=False)
@@ -86,13 +79,12 @@ def get_table(request):
         return HttpResponse("ERROR...")
 
 
-@login_required(login_url="login")
 def get_donation_count_date_qty(request):
-    if request.method == "GET":
+    if (request.method == "GET"):
 
         json_response = {}
         for key in QUERY_DATA.keys():
-            qty_count_by_month = [0 for _ in range(12)]
+            qty_count_by_month = [0 for i in range(12)]
 
             list_of_data = list(get_quantity_group_by_date(QUERY_DATA[key]["MODEL"], "month"))
 
@@ -106,13 +98,12 @@ def get_donation_count_date_qty(request):
         return JsonResponse(json_response, safe=False)
 
 
-@login_required(login_url="login")
 def get_donation_count_month(request):
-    if request.method == "GET":
+    if (request.method == "GET"):
 
         json_response = {}
         for key in QUERY_DATA.keys():
-            qty_count_by_month = [0 for _ in range(12)]
+            qty_count_by_month = [0 for i in range(12)]
 
             list_of_data = list(get_donation_count_by_date(QUERY_DATA[key]["MODEL"], "month"))
 
@@ -126,9 +117,8 @@ def get_donation_count_month(request):
         return JsonResponse(json_response, safe=False)
 
 
-@login_required(login_url="login")
 def get_donation_item_count(request):
-    if request.method == "GET":
+    if (request.method == "GET"):
 
         json_response = {}
         for key in QUERY_DATA.keys():
@@ -142,9 +132,8 @@ def get_donation_item_count(request):
 
 
 # Removed
-@login_required(login_url="login")
 def get_donation_fund_count(request):
-    if request.method == "GET":
+    if (request.method == "GET"):
 
         fund_types = list(FundType.objects.all())
         json_response = {}
@@ -160,50 +149,40 @@ def get_donation_fund_count(request):
         return JsonResponse(results, safe=False)
 
 
-@login_required(login_url="login")
-@transaction.atomic
+
+# @login_required
 def update_item(request):
     if request.method == "POST":
         try:
             ids = []
             update_data = json.loads(request.POST["update_data"])
-            # Sanitize data
-            for k, v in update_data.items():
-                if type(v) not in (int, float, bool):
-                    update_data[k] = remove_html_tags(v)
-
-            print(update_data)
             ids.append(update_data["donor_id"])
             ids.append(update_data["donation_id"])
             ids.append(update_data["item_id"])
             ids.append(update_data["item_id"])
-            with transaction.atomic():
-                print(update_data["date_received"])
-                res = update_item_entry(ids, update_data, request.POST["table_type"])
-                if not res:
-                    res = {"error": "Couldn't update the" + request.POST["table_type"] + " entry, please try again."}
-                    raise Exception("Some exception :(")
-                    # return FailedJsonResponse(res)
+            res = update_item_entry(ids, update_data, request.POST["table_type"])
+            print(res)
+            if not res:
+                res = {"error": "Couldn't update the" + request.POST["table_type"] + " entry, please try again."}
+                return FailedJsonResponse(res)
 
             return JsonResponse(res, safe=False)
 
         except Exception as e:
             print(traceback.format_exc())
             print(f"The error is {e}")
-            return FailedJsonResponse({"error": "Unexpected error occurred while updating data"})
+            return HttpResponse("ERROR...")
 
-    return FailedJsonResponse({"error": "Unknown method"})
+    return HttpResponse("ERROR...")
 
 
-@login_required(login_url="login")
 def delete_item(request):
-    # TODO sanitize data
-    if request.method == "POST":
+    if (request.method == "POST"):
         try:
-            item_id = request.POST["item_id"]
+            id = request.POST["item_id"]
             model = QUERY_DATA[request.POST["table_type"]]["MODEL"]
-            res = delete_item_entry(model, item_id)
-            print(f"{item_id=}")
+            res = delete_item_entry(model, id)
+            print(f"{id=}")
 
             if not res:
                 res = {"error": "Couldn't update the" + request.POST["table_type"] + "entry, please try again."}
